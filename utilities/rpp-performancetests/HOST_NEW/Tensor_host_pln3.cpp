9bb863167f98ef6136348fb2a5e68fe9ada39b70
fiona-gladwin <fionagladwin@multicorewareinc.com>
2022-01-12 10:25:19 -0800

    Add performance test suite changes

--- utilities/rpp-performancetests/HOST_NEW/Tensor_host_pln3.cpp
+++ utilities/rpp-performancetests/HOST_NEW/Tensor_host_pln3.cpp
@@ -24,6 +24,43 @@ typedef half Rpp16f;
 #define RPPMAX2(a,b) ((a > b) ? a : b)
 #define RPPMIN2(a,b) ((a < b) ? a : b)
 
+std::string get_interpolation_type(int val, RpptInterpolationType interpType)
+{
+    switch(val)
+    {
+    case 0:
+        {
+            interpType = RpptInterpolationType::NEAREST_NEIGHBOR;
+            return "nearest_neighbor";
+        }
+        case 2:
+        {
+            interpType = RpptInterpolationType::BICUBIC;
+            return "bicubic";
+        }
+        case 3:
+        {
+            interpType = RpptInterpolationType::LANCZOS;
+            return "lanczos";
+        }
+        case 4:
+        {
+            interpType = RpptInterpolationType::TRIANGULAR;
+            return "triangular";
+        }
+        case 5:
+        {
+            interpType = RpptInterpolationType::GAUSSIAN;
+            return "gaussian";
+        }
+        default:
+        {
+            interpType = RpptInterpolationType::BILINEAR;
+            return "bilinear";
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     // Handle inputs
@@ -36,15 +73,10 @@ int main(int argc, char **argv)
         printf("\nUsage: ./Tensor_host_pln3 <src1 folder> <src2 folder (place same as src1 folder for single image functionalities)> <u8 = 0 / f16 = 1 / f32 = 2 / u8->f16 = 3 / u8->f32 = 4 / i8 = 5 / u8->i8 = 6> <outputFormatToggle (pkd->pkd = 0 / pkd->pln = 1)> <case number = 0:81> <verbosity = 0/1>\n");
         return -1;
     }
-
-    if (atoi(argv[6]) == 1)
+    if ((atoi(argv[5]) == 21) && argc < MIN_ARG_COUNT + 1)
     {
-        printf("\nInputs for this test case are:");
-        printf("\nsrc1 = %s", argv[1]);
-        printf("\nsrc2 = %s", argv[2]);
-        printf("\nu8 / f16 / f32 / u8->f16 / u8->f32 / i8 / u8->i8 (0/1/2/3/4/5/6) = %s", argv[3]);
-        printf("\noutputFormatToggle (pkd->pkd = 0 / pkd->pln = 1) = %s", argv[4]);
-        printf("\ncase number (0:81) = %s", argv[5]);
+        printf("\nUsage: ./Tensor_host_pln3 <src1 folder> <src2 folder (place same as src1 folder for single image functionalities)> <u8 = 0 / f16 = 1 / f32 = 2 / u8->f16 = 3 / u8->f32 = 4 / i8 = 5 / u8->i8 = 6> <outputFormatToggle (pkd->pkd = 0 / pkd->pln = 1)> <case number = 0:81> <interp_type = 0:5> <verbosity = 0/1>\n");
+        return -1;
     }
 
     char *src = argv[1];
@@ -52,6 +84,26 @@ int main(int argc, char **argv)
     int ip_bitDepth = atoi(argv[3]);
     unsigned int outputFormatToggle = atoi(argv[4]);
     int test_case = atoi(argv[5]);
+    unsigned int verbosity = (test_case == 21) ? atoi(argv[7]) : atoi(argv[6]);
+    // Params specific to case 21 - Resize
+    int interpolation_type = (test_case == 21) ? atoi(argv[6]) : 1;
+    RpptInterpolationType interpolationType = RpptInterpolationType::BILINEAR;
+    std::string interpolationTypeName;
+
+    if (verbosity)
+    {
+        printf("\nInputs for this test case are:");
+        printf("\nsrc1 = %s", argv[1]);
+        printf("\nsrc2 = %s", argv[2]);
+        printf("\nu8 / f16 / f32 / u8->f16 / u8->f32 / i8 / u8->i8 (0/1/2/3/4/5/6) = %s", argv[3]);
+        printf("\noutputFormatToggle (pkd->pkd = 0 / pkd->pln = 1) = %s", argv[4]);
+        printf("\ncase number (0:81) = %s", argv[5]);
+        if(test_case == 21)
+        {
+            interpolationTypeName = get_interpolation_type(interpolation_type, interpolationType);
+            printf("\ninterpolation type (0:5) = %s", interpolationTypeName.c_str());
+        }
+    }
 
     int ip_channel = 3;
 
@@ -175,6 +227,11 @@ int main(int argc, char **argv)
     char func[1000];
     strcpy(func, funcName);
     strcat(func, funcType);
+    if(test_case == 21)
+    {
+        strcat(func, "_");
+        strcat(func, interpolationTypeName.c_str());
+    }
 
     char src1[1000];
     strcpy(src1, src);
@@ -719,17 +776,17 @@ int main(int argc, char **argv)
             start_omp = omp_get_wtime();
             start = clock();
             if (ip_bitDepth == 0)
-                rppt_resize_host(input, srcDescPtr, output, dstDescPtr, dstImgSizes, roiTensorPtrSrc, roiTypeSrc, handle);
+                rppt_resize_host(input, srcDescPtr, output, dstDescPtr, dstImgSizes, interpolationType, roiTensorPtrSrc, roiTypeSrc, handle);
             else if (ip_bitDepth == 1)
-                rppt_resize_host(inputf16, srcDescPtr, outputf16, dstDescPtr, dstImgSizes, roiTensorPtrSrc, roiTypeSrc, handle);
+                rppt_resize_host(inputf16, srcDescPtr, outputf16, dstDescPtr, dstImgSizes, interpolationType, roiTensorPtrSrc, roiTypeSrc, handle);
             else if (ip_bitDepth == 2)
-                rppt_resize_host(inputf32, srcDescPtr, outputf32, dstDescPtr, dstImgSizes, roiTensorPtrSrc, roiTypeSrc, handle);
+                rppt_resize_host(inputf32, srcDescPtr, outputf32, dstDescPtr, dstImgSizes, interpolationType, roiTensorPtrSrc, roiTypeSrc, handle);
             else if (ip_bitDepth == 3)
                 missingFuncFlag = 1;
             else if (ip_bitDepth == 4)
                 missingFuncFlag = 1;
             else if (ip_bitDepth == 5)
-                rppt_resize_host(inputi8, srcDescPtr, outputi8, dstDescPtr, dstImgSizes, roiTensorPtrSrc, roiTypeSrc, handle);
+                rppt_resize_host(inputi8, srcDescPtr, outputi8, dstDescPtr, dstImgSizes, interpolationType, roiTensorPtrSrc, roiTypeSrc, handle);
             else if (ip_bitDepth == 6)
                 missingFuncFlag = 1;
             else
