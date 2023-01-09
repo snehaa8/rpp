adf7ea2aa8262ca632c0479252c51d7b78d632cc
r-abishek <abishek@multicorewareinc.com>
2023-01-09 10:44:39 -0800

    Add no-ilp version kernel

--- src/modules/hip/kernel/image_sum.hpp
+++ src/modules/hip/kernel/image_sum.hpp
@@ -102,6 +102,61 @@ __global__ void image_sum_pln1_tensor(T *srcPtr,
 }
 
 template <typename T, typename U>
+__global__ void image_sum_pln1_tensor_without_ilp(T *srcPtr,
+                                                  uint2 srcStridesNH,
+                                                  U *imageSumArr,
+                                                  RpptROIPtr roiTensorPtrSrc)
+{
+    int id_x = (hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x) * 8;
+    int id_y = hipBlockIdx_y * hipBlockDim_y + hipThreadIdx_y;
+    int id_z = hipBlockIdx_z * hipBlockDim_z + hipThreadIdx_z;
+
+    __shared__ float partialSumLDS[16][16];                                     // 16 rows of src, 16 cols of src in a 16 x 16 thread block
+    float *partialSumLDSRowPtr = &partialSumLDS[hipThreadIdx_y][0];             // float pointer to beginning of each row in LDS
+    partialSumLDSRowPtr[hipThreadIdx_x] = 0.0f;                                 // initialization of LDS to 0 using all 16x16 threads
+
+    if ((id_y >= roiTensorPtrSrc[id_z].xywhROI.roiHeight) || (id_x >= roiTensorPtrSrc[id_z].xywhROI.roiWidth))
+    {
+        return;
+    }
+
+    int xAlignedLength = roiTensorPtrSrc[id_z].xywhROI.roiWidth & ~7;           // alignedLength for vectorized global loads
+    int xDiff = roiTensorPtrSrc[id_z].xywhROI.roiWidth - xAlignedLength;        // difference between roiWidth and alignedLength
+    uint srcIdx = (id_z * srcStridesNH.x) + ((id_y + roiTensorPtrSrc[id_z].xywhROI.xy.y) * srcStridesNH.y) + (id_x + roiTensorPtrSrc[id_z].xywhROI.xy.x);
+
+    d_float8 src_f8;
+    rpp_hip_load8_and_unpack_to_float8(srcPtr + srcIdx, &src_f8);               // load 8 pixels to local mmemory
+    if (id_x + 8 > roiTensorPtrSrc[id_z].xywhROI.roiWidth)
+        for(int i = xDiff; i < 8; i++)
+            src_f8.f1[i] = 0.0f;                                                // reset invalid loads to 0.0f
+    partialSumLDSRowPtr[hipThreadIdx_x] += (src_f8.f1[0] + src_f8.f1[1] + src_f8.f1[2] + src_f8.f1[3] + src_f8.f1[4] + src_f8.f1[5] + src_f8.f1[6] + src_f8.f1[7]);    // perform small work of vectorized addition of two f4s and store in LDS
+    __syncthreads();                                                            // syncthreads after LDS load
+
+    // Reduction of 16 floats on 16 threads per block in x dimension (for every y dimension)
+    for (int threadMax = 8; threadMax >= 1; threadMax /= 2)
+    {
+        if (hipThreadIdx_x < threadMax)
+            partialSumLDSRowPtr[hipThreadIdx_x] += partialSumLDSRowPtr[hipThreadIdx_x + threadMax];
+        __syncthreads();
+    }
+
+    if (hipThreadIdx_x == 0)
+    {
+        // Reduction of 16 floats on 16 threads per block in y dimension
+        for (int threadMax = 8, increment = 128; threadMax >= 1; threadMax /= 2, increment /= 2)
+        {
+            if (hipThreadIdx_x < threadMax)
+                partialSumLDSRowPtr[hipThreadIdx_x] += partialSumLDSRowPtr[hipThreadIdx_x + increment];
+            __syncthreads();
+        }
+
+        // Final assignment of single float to dst
+        if (hipThreadIdx_y == 0)
+            imageSumArr[(hipBlockIdx_z * hipGridDim_y + hipBlockIdx_y) * hipGridDim_x + hipBlockIdx_x] = partialSumLDSRowPtr[0];
+    }
+}
+
+template <typename T, typename U>
 RppStatus hip_exec_image_sum_tensor(T *srcPtr,
                                     RpptDescPtr srcDescPtr,
                                     U *imageSumArr,
