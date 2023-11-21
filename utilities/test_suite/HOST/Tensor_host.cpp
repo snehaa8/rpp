ba9b0f445671507149a9eb8392f01b7773804a28
Snehaa Giridharan <snehaa@multicorewareinc.com>
2023-11-20 22:58:10 -0800

    add color temperature

--- utilities/test_suite/HOST/Tensor_host.cpp
+++ utilities/test_suite/HOST/Tensor_host.cpp
@@ -660,6 +660,24 @@ int main(int argc, char **argv)
 
                     break;
                 }
+                case 45:
+                {
+                    testCaseName = "color_temperature";
+
+                    Rpp8s adjustment[batchSize];
+                    for (i = 0; i < batchSize; i++)
+                        adjustment[i] = 70;
+
+                    startWallTime = omp_get_wtime();
+                    startCpuTime = clock();
+                    if (inputBitDepth == 0 || inputBitDepth == 1 || inputBitDepth == 2 || inputBitDepth == 5)
+                        rppt_color_temperature_host(input, srcDescPtr, output, dstDescPtr, adjustment, roiTensorPtrSrc, roiTypeSrc, handle);
+                    else
+                        missingFuncFlag =
+                         1;
+
+                    break;
+                }
                 case 82:
                 {
                     testCaseName = "ricap";
